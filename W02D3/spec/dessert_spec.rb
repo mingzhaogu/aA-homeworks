ab4c89ce93a9957d64e81ab769273997a97ae93e
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-02-20 20:14:27 -0800

    Create dessert spec

--- W02D3/spec/dessert_spec.rb
+++ W02D3/spec/dessert_spec.rb
@@ -6,37 +6,61 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 =end
 
 describe Dessert do
-  let(:chef) { double("chef") }
+  let(:chef) { double("chef", :name => "Jonathan") }
+  subject(:dessert) { Dessert.new("cake", 35, "Jonathan") }
 
   describe "#initialize" do
-    it "sets a type"
+    it "sets a type" do
+      allow(dessert).to receive(:type).and_return("cake")
+    end
 
-    it "sets a quantity"
+    it "sets a quantity" do
+      allow(dessert).to receive(:cake).and_return(35)
+    end
 
-    it "starts ingredients as an empty array"
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
 
-    it "raises an argument error when given a non-integer quantity"
+    it "raises an argument error when given a non-integer quantity" do
+      expect{ Dessert.new("cake", "thirty", "Jonathan") }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("rainbows")
+      expect(dessert.ingredients).to include("rainbows")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      pre_shuffle = dessert.ingredients
+      # dessert.mix!
+      expect(dessert.ingredients).not_to eq(pre_shuffle)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      expect(dessert.eat(5)).to eq(30)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect{ dessert.eat(50) }.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      expect(dessert.serve).to include("Jonathan")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(dessert.make_more).to yield_with_args("cake")
+    end
   end
 end
