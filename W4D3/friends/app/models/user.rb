67132432450d626d007e689ce18428ef4dd182d0
mingzhaogu <anniegu.mingzhao@gmail.com>
2018-03-06 20:59:22 -0800

    Complete auth hw

--- W4D3/friends/app/models/user.rb
+++ W4D3/friends/app/models/user.rb
@@ -11,8 +11,9 @@
 #
 
 class User < ApplicationRecord
-  validates_presence_of :username, :session_token
-  validates_presence_of :password_digest, :message => "Password can't be blank."
+  validates :username, presence: true, uniqueness: true
+  validates :session_token, presence: true, uniqueness: true
+  validates_presence_of :password_digest, presence: { message: "Password can't be blank." }
   validates_presence_of :password, length: { minimum: 6, allow_nil: true }
 
   before_validation :ensure_session_token
@@ -22,20 +23,18 @@ class User < ApplicationRecord
   end
 
   def self.find_by_credentials(username, password)
-    unless self.find_by(username: username) && self.is_password?(password)
-      return false
-    end
+    user = self.find_by(username: username)
+    return user if user.is_password?(password)
   end
 
   def reset_session_token!
-    self.session_token = nil
+    self.session_token = User.generate_session_token
     self.save!
+    self.session_token
   end
 
   def ensure_session_token
-    if session_token.nil?
-      self.session_token = generate_session_token
-    end
+    self.session_token ||= generate_session_token
   end
 
   def password=(password)
---
+++ W4D3/friends/db/migrate/20180307045430_add_index_for_session_tokens.rb
@@ -0,0 +1,5 @@
+class AddIndexForSessionTokens < ActiveRecord::Migration[5.1]
+  def change
+    add_index :users, :session_token, unique: true
+  end
+end
